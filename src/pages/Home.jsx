b45d25abb3e281f145614d4856cfbf3385ecbda1
Hyesu Kim <khs010305@naver.com>
2025-05-11 17:56:56 +0900

    second commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,7 @@ const Home = () => {
         <button className="howto_button" onClick={() => setIsHowtoOpen(true)}>
           게임방법
         </button>
+        <h3>추가햇음</h3>
       </section>
 
       {isHowtoOpen && (
