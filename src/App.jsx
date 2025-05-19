bdb17e7cfda2acc8b6d5c7650aafba21a621f21b
lsuinl <su96in43@naver.com>
2025-05-19 19:18:01 +0900

    feat: 사운드 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import "./styles/App.css";
 import { Routes, Route, useNavigate } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import Home from "./pages/home/Home";
 import Game from "./pages/game/Game";
 import Result from "./pages/result/Result";
 import Notfound from "./pages/notfound/Notfound";
 
+function App() {
+  const audioRef = useRef(null);
+  const [played, setIsPlayed] = useState(false);
+
+  useEffect(() => {
+  const soundClick = () => {
+    if (!played && audioRef.current) {
+      audioRef.current.play().then(() => {
+        setIsPlayed(true);
+      });
+    }
+  };
+  document.addEventListener('click', soundClick, { once: true });
+}, [played]);
+
+
 // 1. "/" : 메인화면 - Home 페이지
 // 2. "/game" : 게임화면 - Game 페이지
 // 3. "/result" : 결과화면 - Result 페이지지
-function App() {
   return (
     <>
+      <audio ref={audioRef} src="/assets/sound/main.mp3" loop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
